54c39b0740a3825c65e5db1b9018f779433cd881
Nalin Hettiarachchi <nalinsk2001@gmail.com>
2026-02-01 16:06:26 -0500

    first commit

---
+++ src/main/java/com/netsofit/adultcare/AdultCareApplication.java
@@ -0,0 +1,13 @@
+package com.netsofit.adultcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdultCareApplication.class, args);
+    }
+
+}
